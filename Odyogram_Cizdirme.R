d46ea12c2f5524bcd44b2854858fde87184254c4
SonerTrd <87863167+SonerTrd@users.noreply.github.com>
2022-09-02 14:55:50 +0200

    Add files via upload

---
+++ Odyogram_Cizdirme.R
@@ -0,0 +1,223 @@
+library(shiny)
+library(ggplot2)
+
+# read full ISO dataset
+isofull <- read.csv("ISO2017full.csv", header=TRUE)
+
+ui <- fluidPage(
+
+  fluidRow(
+    style = "font-size: 1.2em; padding: 1em 1em; margin:0em", 
+    tags$b(p("Yonerge")),
+    p("Her bir frekans icin asagidaki bosluklari doldurun ya da yukari/asagi ok ile degerini giriniz. Her kutuya -10 ile 100 (ikisi de dahil) arasinda bir deger girebilirsiniz. Eger o frekansta bir deger yok ise bos birakiniz."),    
+    p("Eger katilimciya ait isitme esiklerini normatif verilerle karsilastirmak isterseniz, 'Plot ISO percentiles' sekmesine tiklayiniz ve katilimciya ait yas ile cinsiyeti dogru giriniz. Normatif yuzdelik dilimlere ait veriler ISO 7029:2017 veriseti tablo D1'den alinmistir."),  
+      "Yuzdelik degerler 80 yas ustu katilimcilar icin 3000 Hz ile 8000 Hz arasindaki degerler icin sadece bilgi amaclidir."
+    ),
+  
+  sidebarLayout(
+  
+    sidebarPanel(style = "background-color: white", 
+                 
+      fluidRow(column(width = 4, align = "center", style = "font-size: 1.25em; color: blue", "Left ear"), 
+               column(width = 4, align = "center", style = "font-size: 1.25em; color: black", "Frequency"), 
+               column(width = 4, align = "center", style = "font-size: 1.25em; color: red", "Right ear")
+               ),  
+      
+      br(),
+      
+      fluidRow(column(width = 4, numericInput(inputId = "L250", label = NULL, value = NULL, step = 5, min = -10, max = 100)),
+               column(width = 4, align = "center", style = "font-size: 1.25em; padding-top: 5px", "250"), 
+               column(width = 4, numericInput(inputId = "R250", label = NULL, value = NULL, step = 5, min = -10, max = 100)) 
+               ), 
+  
+      fluidRow(column(width = 4, numericInput(inputId = "L500", label = NULL, value = NULL, step = 5, min = -10, max = 100)),
+               column(width = 4, align = "center", style = "font-size: 1.25em; padding-top: 5px", "500"),
+               column(width = 4, numericInput(inputId = "R500", label = NULL, value = NULL, step = 5, min = -10, max = 100))
+               ), 
+      
+      fluidRow(column(width = 4, numericInput(inputId = "L1000", label = NULL, value = NULL, step = 5, min = -10, max = 100)),
+               column(width = 4, align = "center", style = "font-size: 1.25em; padding-top: 5px", "1000"),
+               column(width = 4, numericInput(inputId = "R1000", label = NULL, value = NULL, step = 5, min = -10, max = 100))
+               ), 
+  
+      fluidRow(column(width = 4, numericInput(inputId = "L2000", label = NULL, value = NULL, step = 5, min = -10, max = 100)),
+               column(width = 4, align = "center", style = "font-size: 1.25em; padding-top: 5px", "2000"),
+               column(width = 4, numericInput(inputId = "R2000", label = NULL, value = NULL, step = 5, min = -10, max = 100))
+               ), 
+  
+      fluidRow(column(width = 4, numericInput(inputId = "L3000", label = NULL, value = NULL, step = 5, min = -10, max = 100)),
+               column(width = 4, align = "center", style = "font-size: 1.25em; padding-top: 5px", "3000"),
+               column(width = 4, numericInput(inputId = "R3000", label = NULL, value = NULL, step = 5, min = -10, max = 100))
+               ), 
+  
+      fluidRow(column(width = 4, numericInput(inputId = "L4000", label = NULL, value = NULL, step = 5, min = -10, max = 100)),
+               column(width = 4, align = "center", style = "font-size: 1.25em; padding-top: 5px", "4000"),
+               column(width = 4, numericInput(inputId = "R4000", label = NULL, value = NULL, step = 5, min = -10, max = 100))
+               ), 
+      
+      fluidRow(column(width = 4, numericInput(inputId = "L6000", label = NULL, value = NULL, step = 5, min = -10, max = 100)),
+               column(width = 4, align = "center", style = "font-size: 1.25em; padding-top: 5px", "6000"),
+               column(width = 4, numericInput(inputId = "R6000", label = NULL, value = NULL, step = 5, min = -10, max = 100))
+               ), 
+  
+      fluidRow(column(width = 4, numericInput(inputId = "L8000", label = NULL, value = NULL, step = 5, min = -10, max = 100)),
+               column(width = 4, align = "center", style = "font-size: 1.25em; padding-top: 5px", "8000"),
+               column(width = 4, numericInput(inputId = "R8000", label = NULL, value = NULL, step = 5, min = -10, max = 100))
+               ), 
+      
+      
+      hr(), 
+      
+      fluidRow(column(width = 8, style = "font-size: 1.25em", checkboxInput(inputId = "iso", label = tags$b("Plot ISO percentiles"), value = FALSE))
+               ),
+  
+      fluidRow(column(width = 1, align = "center", style = "padding-top: 6px", tags$b("Sex")), 
+               column(width = 4, selectInput(inputId = "sex", label = NULL, choices = c("Female", "Male"))),
+               column(width = 1, ""), 
+               column(width = 1, align = "center", style = "padding-top: 6px", tags$b("Age")), 
+               column(width = 4, selectInput(inputId = "age", label = NULL, choices = c("20", "30", "40", "50", "60", "70", "80")))
+               )
+      
+      ),
+
+  mainPanel(
+    plotOutput("odyogram", width = 800, height = 600)
+      )
+  
+  ), 
+
+    
+  fluidRow(style = "padding: 1em 1em 2em", 
+           column(width = 2, downloadButton(outputId = "downloadPDF", label = "Download PDF")),
+           column(width = 2, downloadButton(outputId = "downloadPNG", label = "Download PNG")),
+           column(width = 8, "")
+           ),    
+  
+)
+
+server <- function(input, output){
+  
+  # Collect threshold inputs into a dataframe
+  thresholds.L.initial <- reactive({data.frame(freq = c(250, 500, 1000, 2000, 3000, 4000, 6000, 8000),
+                                          threshold = c(input$L250, input$L500, input$L1000, input$L2000, input$L3000, input$L4000, input$L6000, input$L8000))
+                              })
+  
+  thresholds.R.initial <- reactive({data.frame(freq = c(250, 500, 1000, 2000, 3000, 4000, 6000, 8000),
+                                          threshold = c(input$R250, input$R500, input$R1000, input$R2000, input$R3000, input$R4000, input$R6000, input$R8000))
+                              })
+  
+  # Limit the dataframe to frequencies with non-null threshold values
+  thresholds.L.final <- reactive({thresholds.L.initial()[thresholds.L.initial()$threshold != "", ]
+                              })
+
+  thresholds.R.final <- reactive({thresholds.R.initial()[thresholds.R.initial()$threshold != "", ]
+                              })
+  
+  # Select appropriate ISO data based on sex and age, in case percentiles are required
+  iso <- reactive({
+    subset(isofull, Sex == input$sex & Age == input$age)
+    })
+  
+  # Plot one of two versions
+  plotInput <- reactive({
+  
+    if (input$iso == FALSE) {
+ 
+    # Plot client audiogram only
+    ggplot() +        
+    # Plot right ear first
+    geom_line(data = thresholds.R.final(), aes(x = freq, y = threshold), size = 0.6, linetype = "solid", colour = "red") + 
+    geom_point(data = thresholds.R.final(), aes(x = freq, y = threshold), size = 5, stroke = 1.5, shape = 1, colour = "red") +
+    geom_text(data = thresholds.R.final(), aes(label = "R", x = (freq[1] - 25), y = threshold[1]), color = "red", size = 5) + 
+    # Plot left ear second
+    geom_line(data = thresholds.L.final(), aes(x = freq, y = threshold), size = 0.6, linetype = "longdash", colour = "blue") + 
+    geom_point(data = thresholds.L.final(), aes(x = freq, y = threshold), size = 5, stroke = 1.5, shape = 4, colour = "blue") +
+    geom_text(data = thresholds.L.final(), aes(label = "L", x = (freq[1] - 25), y = threshold[1]), color = "blue", size = 5) + 
+    # Other plot parameters         
+    labs(x = "Frequency (Hz)", y = "Threshold (dB HL)") +
+    scale_y_reverse(limits = c(100, -10), breaks = seq(-10, 100, by = 10)) + 
+    scale_x_log10(limits = c(200, 10000), breaks = c(250, 500, 1000, 2000, 3000, 4000, 6000, 8000), labels = c('250', '500', '1000', '2000', '' , '4000', '', '8000')) +
+    theme_bw() + 
+    theme(axis.title = element_text(size = 25), 
+      axis.text = element_text(colour = "black", size = 20), 
+      text = element_text(size = 20)) + 
+    theme(panel.background = element_rect(color = "black", size = 1)) + 
+    theme(legend.position = c(0.1, 0.2)) + 
+    theme(legend.background = element_rect(fill = "white", color = "black"))
+      
+    } else {
+    
+    # Plot ISO percentiles in light grey lines, then client audiogram
+    
+      # Make percentile labels bigger as percentile lines are spaced further apart
+      if (input$age == "20") {customsize <- 3}
+      else if (input$age == "30") {customsize <- 4}
+      else {customsize <- 5}
+    
+    ggplot() + 
+    # Plot ISO lines first
+    geom_line(data = iso(), aes(x = Frequency, y = Percentile90), color = "darkgray", linetype = "solid") + 
+    geom_line(data = iso(), aes(x = Frequency, y = Percentile75), color = "darkgray", linetype = "solid") + 
+    geom_line(data = iso(), aes(x = Frequency, y = Percentile50), color = "darkgray", linetype = "solid") + 
+    geom_line(data = iso(), aes(x = Frequency, y = Percentile25), color = "darkgray", linetype = "solid") + 
+    geom_line(data = iso(), aes(x = Frequency, y = Percentile10), color = "darkgray", linetype = "solid") + 
+    geom_text(data = iso(), aes(label = "90", x = 8600, y = Percentile90[8]), color = "darkgray", size = customsize) +
+    geom_text(data = iso(), aes(label = "75", x = 8600, y = Percentile75[8]), color = "darkgray", size = customsize) +
+    geom_text(data = iso(), aes(label = "50", x = 8600, y = Percentile50[8]), color = "darkgray", size = customsize) +
+    geom_text(data = iso(), aes(label = "25", x = 8600, y = Percentile25[8]), color = "darkgray", size = customsize) +
+    geom_text(data = iso(), aes(label = "10", x = 8600, y = Percentile10[8]), color = "darkgray", size = customsize) +
+    # Add client audiogram
+    geom_line(data = thresholds.R.final(), aes(x = freq, y = threshold), size = 0.6, linetype = "solid", colour = "red") + 
+    geom_point(data = thresholds.R.final(), aes(x = freq, y = threshold), size = 5, stroke = 1.5, shape = 1, colour = "red") +
+    geom_text(data = thresholds.R.final(), aes(label = "R", x = (freq[1] - 25), y = threshold[1]), color = "red", size = 5) +  
+    geom_line(data = thresholds.L.final(), aes(x = freq, y = threshold), size = 0.6, linetype = "longdash", colour = "blue") + 
+    geom_point(data = thresholds.L.final(), aes(x = freq, y = threshold), size = 5, stroke = 1.5, shape = 4, colour = "blue") +
+    geom_text(data = thresholds.L.final(), aes(label = "L", x = (freq[1] - 25), y = threshold[1]), color = "blue", size = 5) +
+    labs(x = "Frequency (Hz)", y = "Threshold (dB HL)") +
+    scale_y_reverse(limits = c(100, -10), breaks = seq(-10, 100, by = 10)) + 
+    scale_x_log10(limits = c(200, 10000), breaks = c(250, 500, 1000, 2000, 3000, 4000, 6000, 8000), labels = c('250', '500', '1000', '2000', '', '4000', '', '8000')) +
+    theme_bw() + 
+    theme(axis.title = element_text(size = 25), 
+      axis.text = element_text(colour = "black", size = 20), 
+      text = element_text(size = 20)) + 
+    theme(panel.background = element_rect(color = "black", size = 1)) + 
+    theme(legend.position = c(0.1, 0.2)) + 
+    theme(legend.background = element_rect(fill = "white", color = "black"))
+      
+    }    
+    
+  })
+  
+  # Display plot in user interface  
+  output$odyogram <- renderPlot({ 
+    plotInput()
+    })
+  
+  # Save plot to desktop as PDF
+  output$downloadPDF <- downloadHandler(
+      filename = function() { 
+        paste0("odyogram-", Sys.Date(), ".pdf") 
+        },
+      content = function(file) { 
+        pdf(file, width = 8, height = 6)
+        print(plotInput())
+        dev.off()
+        contentType = 'pdf'
+        }
+      )
+
+  # Save plot to desktop as PNG
+  output$downloadPNG <- downloadHandler(
+      filename = function() { 
+        paste0("odyogram-", Sys.Date(), ".png", sep='') },
+      content = function(file) { 
+        png(file, width = 800, height = 600, res= NA)
+        print(plotInput())
+        dev.off()
+        contentType = 'image/png'
+        }
+      )
+
+}
+
+shinyApp(ui, server)
---
+++ README.md
@@ -0,0 +1,17 @@
+## Proje hakkinda ##
+ 
+Bu proje ile istediginiz Odyogrami cizdirebilir ve PNG ile PDF formatlarinda indirebilirsiniz.
+Basit olarak orta sol kosede, sag ve sol kulak icin 250 Hz ile 8000 Hz arasindaki degerleri girerek, hemen alt kisimda ISO normatif degerlerini cizdirerek grup karsilastirmasi yapabilirsiniz.
+ 
+## Online olarak calistirmak isterseniz ##
+ 
+Web uygulamasini isterseniz kendi websitenize yukleyebilirsiniz, isterseniz de asagidaki link uzerinden erisebilirsiniz.
+[https://audi0logist.shinyapps.io/Odyogram_Cizdirme/](https://audi0logist.shinyapps.io/Odyogram_Cizdirme/)
+ 
+## Running the app offline (on a local computer) ##
+ 
+R scriptine odyogram_cizdirme.R sekline ulasabilirsiniz.
+ 
+Scripti actiktan sonra 'shiny' ve 'ggplot2' kutuphaneleri yuklenmesi gerekiyor.
+ISO normatif verilerinin ayni klasorde olduguna dikkat ediniz.
+Daha sonra sag ustten 'runapp' diyerek uygulamayi calistirabilirsiniz.
